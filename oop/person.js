b9631a0c01b5e850fd3c8fed34a83c3b71ff5c5a
Kevin Schneider <schneider.kev@outlook.de>
2021-05-15 11:52:48 +0200

    finish prototype intro

--- oop/person.js
+++ oop/person.js
@@ -1,9 +1,35 @@
-const Person = function (fn, ln, age) {
+const Person = function (fn, ln, age, likes) {
     this.firstName = fn,
     this.lastName = ln,
-    this.age = age
+    this.age = age,
+    this.likes = likes
 }
 
 const me = new Person("kev", "schne", 26)
 
 console.log(me) 
+
+// prototypal inheritance
+Person.prototype.getBio = function() {
+    let bio = `${this.firstName} ${this.lastName} is ${this.age}`
+
+    this.likes.forEach(element => {
+        bio += `\r\n\t${this.firstName} likes ${element}` // arrow functions do not bind `this` -> this is the person from the above scope.
+    })
+
+    return bio
+
+}
+
+Person.prototype.setName = function(fullName) {
+    let names = fullName.split(" ")
+    this.firstName = names[0]
+    this.lastName = names[1]
+}
+
+const mew = new Person("kev", "schne", 26, ["sose", "meem"])
+
+
+console.log(mew.getBio()) 
+mew.setName("soos saAS")
+console.log(mew.getBio()) 
