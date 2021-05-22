cb4f374a91604f1ddf42f2dd2afc5859252375a7
Kevin Schneider <schneider.kev@outlook.de>
2021-05-22 14:31:35 +0200

    rewrite person with class syntax

--- oop/person.js
+++ oop/person.js
@@ -1,8 +1,28 @@
-const Person = function (fn, ln, age, likes) {
-    this.firstName = fn,
-    this.lastName = ln,
-    this.age = age,
-    this.likes = likes
+class Person {
+    constructor (fn, ln, age, likes) {
+        this.firstName = fn,
+        this.lastName = ln,
+        this.age = age,
+        this.likes = likes
+    }
+
+    getBio () {
+        let bio = `${this.firstName} ${this.lastName} is ${this.age}`
+    
+        this.likes.forEach(element => {
+            bio += `\r\n\t${this.firstName} likes ${element}` // arrow functions do not bind `this` -> this is the person from the above scope.
+        })
+    
+        return bio
+        
+    }
+    
+    setName(fullName) {
+        let names = fullName.split(" ")
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
+
 }
 
 const me = new Person("kev", "schne", 26)
@@ -10,22 +30,6 @@ const me = new Person("kev", "schne", 26)
 console.log(me) 
 
 // prototypal inheritance
-Person.prototype.getBio = function() {
-    let bio = `${this.firstName} ${this.lastName} is ${this.age}`
-
-    this.likes.forEach(element => {
-        bio += `\r\n\t${this.firstName} likes ${element}` // arrow functions do not bind `this` -> this is the person from the above scope.
-    })
-
-    return bio
-
-}
-
-Person.prototype.setName = function(fullName) {
-    let names = fullName.split(" ")
-    this.firstName = names[0]
-    this.lastName = names[1]
-}
 
 const mew = new Person("kev", "schne", 26, ["sose", "meem"])
 
